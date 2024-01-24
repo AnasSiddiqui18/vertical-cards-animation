1a4b264db2aab19864064810679c18a38d52c3f2
Anas Siddiqui <anassiddiqui0910@gmail.com>
2024-01-25 00:22:19 +0500

    remove slice

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,12 @@ function App() {
       <div className="min-h-screen bg-gray-950 overflow-hidden">
         <div className="flex flex-col h-screen w-full rotate-6">
           <div className="aa w-full h-full flex flex-col">
-            {slicedData.slice(0, 15).map((anime, index) => {
+            {slicedData.map((anime, index) => {
               return <AnimeCard anime={anime} key={index} />;
             })}
             {/* Make sure to duplicate the cards, it will create a illusion of infinite loop */}
             {/* I also removed many cards from Data.jsx and made sure that there was 12 Cards */}
-            {slicedData.slice(0, 15).map((anime, index) => {
+            {slicedData.map((anime, index) => {
               return <AnimeCard anime={anime} key={index} />;
             })}
           </div>
