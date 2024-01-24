4aca03fabac37614f3ef63baa8b9f8de2d21c2dc
MI Sajid <misajidsa@gmail.com>
2024-01-24 18:40:32 +0600

    fixed the infinite scroll animation

--- src/AnimeCard.jsx
+++ src/AnimeCard.jsx
@@ -3,47 +3,21 @@ import { useEffect, useRef } from "react";
 import { Linear, gsap } from "gsap";
 
 const AnimeCard = ({ anime }) => {
-  const cardsRef = useRef(null);
-
-  useEffect(() => {
-    const tl = gsap.timeline({ repeat: -1 }); // Infinite loop with yoyo effect
-
-    tl.to(cardsRef.current.querySelectorAll("img"), {
-      y: -100, // Move upwards
-      duration: 2,
-      ease: Linear,
-      yoyo: false,
-    });
-  }, []);
-  return (
-    <div
-      className="flex gap-x-8 justify-end py-5 mr-5 transition-all"
-      id="victim"
-      ref={cardsRef}
-    >
-      <div className="flex anime-card" id="victim_01">
-        <img
-          className="w-[200px] rounded-md shadow-md shadow-orange-300"
-          src={anime.image.original}
-          alt="#"
-        />
-      </div>
-      <div className="flex anime-card" id="victim_02">
-        <img
-          className="w-[200px] rounded-md shadow-md shadow-orange-300"
-          src={anime.image.original}
-          alt=""
-        />
-      </div>
-      <div className="flex anime-card" id="victim_03">
-        <img
-          className="w-[200px] rounded-md shadow-md shadow-orange-300"
-          src={anime.image.original}
-          alt=""
-        />
-      </div>
-    </div>
-  );
+    const cardsRef = useRef(null);
+    return (
+        // Gave a fixed height of 33.3333%, it may flicker a little bit.
+        <div className="flex h-1/3 gap-x-8 justify-end py-5 mr-5 transition-all" id="victim" ref={cardsRef}>
+            <div className="flex anime-card" id="victim_01">
+                <img className="w-[200px] rounded-md shadow-md shadow-orange-300" src={anime.image.original} alt="#" />
+            </div>
+            <div className="flex anime-card" id="victim_02">
+                <img className="w-[200px] rounded-md shadow-md shadow-orange-300" src={anime.image.original} alt="" />
+            </div>
+            <div className="flex anime-card" id="victim_03">
+                <img className="w-[200px] rounded-md shadow-md shadow-orange-300" src={anime.image.original} alt="" />
+            </div>
+        </div>
+    );
 };
 
 export default AnimeCard;
