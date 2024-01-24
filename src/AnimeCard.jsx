7ed75db80dc12193a3c8681f13a44fdf15c3bf07
Anas Siddiqui <anassiddiqui0910@gmail.com>
2024-01-24 18:02:10 +0500

    modify Anime Card

--- src/AnimeCard.jsx
+++ src/AnimeCard.jsx
@@ -1,23 +1,38 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useRef } from "react";
-import { Linear, gsap } from "gsap";
+import { useRef } from "react";
 
 const AnimeCard = ({ anime }) => {
-    const cardsRef = useRef(null);
-    return (
-        // Gave a fixed height of 33.3333%, it may flicker a little bit.
-        <div className="flex h-1/3 gap-x-8 justify-end py-5 mr-5 transition-all" id="victim" ref={cardsRef}>
-            <div className="flex anime-card" id="victim_01">
-                <img className="w-[200px] rounded-md shadow-md shadow-orange-300" src={anime.image.original} alt="#" />
-            </div>
-            <div className="flex anime-card" id="victim_02">
-                <img className="w-[200px] rounded-md shadow-md shadow-orange-300" src={anime.image.original} alt="" />
-            </div>
-            <div className="flex anime-card" id="victim_03">
-                <img className="w-[200px] rounded-md shadow-md shadow-orange-300" src={anime.image.original} alt="" />
-            </div>
-        </div>
-    );
+  const cardsRef = useRef(null);
+  return (
+    // Gave a fixed height of 33.3333%, it may flicker a little bit.
+    <div
+      className="flex h-1/3 gap-x-8 justify-end py-5 mr-5 transition-all"
+      id="victim"
+      ref={cardsRef}
+    >
+      <div className="flex anime-card" id="victim_01">
+        <img
+          className="w-[200px] rounded-md shadow-md shadow-orange-300"
+          src={anime.image.original}
+          alt="#"
+        />
+      </div>
+      <div className="flex anime-card" id="victim_02">
+        <img
+          className="w-[200px] rounded-md shadow-md shadow-orange-300"
+          src={anime.image.original}
+          alt=""
+        />
+      </div>
+      <div className="flex anime-card" id="victim_03">
+        <img
+          className="w-[200px] rounded-md shadow-md shadow-orange-300"
+          src={anime.image.original}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AnimeCard;
